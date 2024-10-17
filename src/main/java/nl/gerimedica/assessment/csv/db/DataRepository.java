cdde4480439987581575800f10f0799339d1f2e7
Lukas Klinghammer <lklingha@stud.hs-heilbronn.de>
2024-10-17 15:53:26 +0200

    Add CSV upload and query functionalities with tests
    Implemented CSV file upload, parsing, and data storage in the `DataRecordFacade`. Added methods to fetch all records, fetch by code with error handling, and delete all data. Introduced JPA repository for database operations and created unit tests to verify the functionalities.
    
    tests are currently still failing as the data is not written to the DB

--- src/main/java/nl/gerimedica/assessment/csv/db/DataRepository.java
+++ src/main/java/nl/gerimedica/assessment/csv/db/DataRepository.java
@@ -4,6 +4,10 @@ import nl.gerimedica.assessment.csv.impl.record.DataRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DataRepository extends JpaRepository<DataRecord, String> {
+
+    List<DataRecord> findByCode(String code);
 }
--- src/main/java/nl/gerimedica/assessment/csv/impl/DataRecordFacade.java
+++ src/main/java/nl/gerimedica/assessment/csv/impl/DataRecordFacade.java
@@ -1,30 +1,76 @@
 package nl.gerimedica.assessment.csv.impl;
 
+import io.micrometer.common.util.StringUtils;
+import nl.gerimedica.assessment.common.ResourceNotFoundException;
 import nl.gerimedica.assessment.csv.api.DataRecordService;
+import nl.gerimedica.assessment.csv.db.DataRepository;
 import nl.gerimedica.assessment.csv.impl.record.DataRecord;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.stream.Collectors;
+
 
 public class DataRecordFacade implements DataRecordService {
 
+    @Autowired
+    private DataRepository dataRepository;
+
     @Override
     public void uploadData(File csvFile) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(csvFile))) {
+            List<DataRecord> records = reader.lines().skip(1)
+                    .map(line -> {
+                        String[] fields = line.split(",");
 
+                        LocalDate fromDate = generateLocalDateFromString(fields[5]);
+                        LocalDate toDate = generateLocalDateFromString(fields[6]);
+
+                        return new DataRecord(0,
+                                fields[0],
+                                fields[1],
+                                fields[2],
+                                fields[3],
+                                fields[4],
+                                fromDate,
+                                toDate,
+                                fields[7]);
+                    })
+                    .collect(Collectors.toList());
+
+            dataRepository.saveAll(records);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to parse CSV file", e);
+        }
     }
 
     @Override
     public List<DataRecord> fetchAllData() {
-        return List.of();
+        return dataRepository.findAll();
     }
 
     @Override
-    public List<DataRecord> fetchByCode(String code) {
-        return List.of();
+    public List<DataRecord> fetchByCode(String code) throws ResourceNotFoundException {
+        List<DataRecord> dataRecordsByCode = dataRepository.findByCode(code);
+        if (dataRecordsByCode.isEmpty()) {
+            throw new ResourceNotFoundException("No records found for code " + code + ".");
+        } else {
+            return dataRecordsByCode;
+        }
     }
 
     @Override
     public void deleteAllData() {
+        dataRepository.deleteAll();
+    }
 
+    private LocalDate generateLocalDateFromString(String stringFromFile) {
+        stringFromFile = stringFromFile.replace("\"", "");
+        return StringUtils.isEmpty(stringFromFile) ? null : LocalDate.parse(stringFromFile, DateTimeFormatter.ofPattern("dd-MM-yyyy"));
     }
 }
